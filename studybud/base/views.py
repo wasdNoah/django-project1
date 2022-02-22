e3bc96396d23b9c3b8c21f8b39e1ad1e7c738633
Noé Cuyuch <58575608+wasdNoah@users.noreply.github.com>
2022-02-21 19:45:20 -0600

    First interface views

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,4 +1,5 @@
 from email import message
+from multiprocessing import context
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
@@ -80,18 +81,19 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
     
     context = {
         'rooms': rooms,
-            
         'topics': topics,
-        'room_count': room_count
+        'room_count': room_count,
+        'room_messages': room_messages
     }
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('-created')
+    room_messages = room.message_set.all()
     participants = room.participants.all()
 
     if request.method == 'POST':
@@ -111,6 +113,20 @@ def room(request, pk):
         }
     return render(request, 'base/room.html', context)
 
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    
+    context = {
+        'user': user,
+        'rooms': rooms,
+        'topics': topics,
+        'room_messages': room_messages
+        }
+    return render(request, 'base/profile.html', context)
+
 @login_required(login_url='/login')
 def createRoom(request):
     form = RoomForm()
@@ -118,7 +134,9 @@ def createRoom(request):
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect('home')
     
     context = {'form': form}
